2dc6960c39bffd68da95bf4b865a026911762557
takerukyono <respect-flea@i.softbank.jp>
2021-03-29 02:09:41 +0900

    Finish Section 16

---
+++ Kisodakemanabu_PHP_Programmingkouza/webapp/bar/a.php
@@ -0,0 +1,13 @@
+<?php
+$title = "A PAGE";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Sample</title>
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+</body>
+</html>
---
+++ Kisodakemanabu_PHP_Programmingkouza/webapp/web_tr1.php
@@ -0,0 +1,14 @@
+<?php
+$date = date("Y-m-d H:i:s");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Sample</title>
+</head>
+<body>
+    <h1><?php echo($date); ?></h1>
+</body>
+</html>
